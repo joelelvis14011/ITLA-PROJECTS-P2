1e072186a9d099fb4e0ef0a3e110521fcab3400f
Joel Antonio Sanchez Rojas <Joelelvis14011@gmail.com>
2026-03-21 20:14:57 -0400

    Homework3 - CleanCity Domain and Infrastructure

--- CleanCity.Application/Dtos/Operator/OperatorCreateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CleanCity.Application.Dtos.Operator
-{
-    public class OperatorCreateDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Position { get; set; } = string.Empty;
-        public bool IsActive { get; set; }
-    }
-}
